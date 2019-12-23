d86a2284b39699f6e286b8c4fc22dd286d3444a2
hbrice <hn.brice@gmail.com>
2019-12-23 10:16:37 -0800

    updating files to modules for mocha tests

--- caesar.js
+++ caesar.js
@@ -8,53 +8,65 @@
 * @returns {encodedWord: String} Returns the new endocded word
 *
 */
-function cipherText(word, shiftCount, shiftLeft){
-	const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-	const maxAlphabet = 25;
 
-	let encodedWord = ""; 	// String
-	let letter = ""; 	// String
-	let encodedLetter = ""; // String
-	let index = 0;		// Number
-	let encodedIndex = 0;	// Number
+var Ciphers = function (){
 
-	// seperate each letter
-	let letterArray = word.split('');
-	
-	// find the index of the letter in the alphabet
-	while (letterArray.length > 0){
+	var cipherText = function(word, shiftCount, shiftLeft){
+		const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+		const maxAlphabet = 25;
 
-		// get first letter of word
-		letter = letterArray[0];
+		let encodedWord = ""; 	// String
+		let letter = ""; 	// String
+		let encodedLetter = ""; // String
+		let index = 0;		// Number
+		let encodedIndex = 0;	// Number
+
+		// seperate each letter
+		let letterArray = word.split('');
 		
-		// get index of letter
-		index = alphabet.indexOf(letter);
+		// find the index of the letter in the alphabet
+		while (letterArray.length > 0){
 
-		if(shiftLeft){
-			// apply the left shift to the index
-			encodedIndex = index - shiftCount;
-		}else {
-			// apply the right shift to the index
-			encodedIndex = index + shiftCount;
+			// get first letter of word
+			letter = letterArray[0];
+			
+			// get index of letter
+			index = alphabet.indexOf(letter);
+
+			if(shiftLeft){
+				// apply the left shift to the index
+				encodedIndex = index - shiftCount;
+			}else {
+				// apply the right shift to the index
+				encodedIndex = index + shiftCount;
+			}
+			
+			//edge case wrapping alaphabet
+			if (encodedIndex < 0 ){
+				encodedIndex = maxAlphabet + encodedIndex;
+			}else if(encodedIndex > maxAlphabet){
+				encodedIndex = encodedIndex - maxAlphabet;
+			}
+
+			// take the index and shift to get the new letter
+			encodedLetter = alphabet[encodedIndex];
+
+			// add new letter to encodedWord
+			encodedWord = encodedWord + encodedLetter;
+
+			// remove first letter of letterArray
+			letterArray.shift();
 		}
-		
-		//edge case wrapping alaphabet
-		if (encodedIndex < 0 ){
-			encodedIndex = maxAlphabet + encodedIndex;
-		}else if(encodedIndex > maxAlphabet){
-			encodedIndex = encodedIndex - maxAlphabet;
-		}
 
-		// take the index and shift to get the new letter
-		encodedLetter = alphabet[encodedIndex];
+		// return encoded word
+		return encodedWord;
+	};
+	/*
+	* End private functions
+	*/
+	return {
+		cipherText: cipherText
+	};
+};
 
-		// add new letter to encodedWord
-		encodedWord = encodedWord + encodedLetter;
-
-		// remove first letter of letterArray
-		letterArray.shift();
-	}
-
-	// return encoded word
-	return encodedWord;
-}
+module.exports = new Ciphers();
--- test.js
+++ test.js
@@ -1,38 +1,41 @@
 //unit tests for caesar cipher
+const caesar = require('./caesar.js')
+
 const assert = require('assert')
+
 
 it('should return true', () => {
   assert.equal(true,true)
 })
 
 it('should return a value', () => {
-	assert.equeal(cipherText('lightsaber', 5, true), 'gdbconuvym');
+	assert.equal(caesar.cipherText('lightsaber', 5, true), 'gdbconuvym');
 })
 
 it('should return a value', () => {
-	assert.equeal(cipherText('lightsaber', 5, false), 'qnlmyxfgjw');
+	assert.equal(cipherText('lightsaber', 5, false), 'qnlmyxfgjw');
 })
 
 it('should return a value', () => {
-	assert.equeal(cipherText('world', 1, false), 'xpsme');
+	assert.equal(cipherText('world', 1, false), 'xpsme');
 })
 
 it('should return a value', () => {
-	assert.equeal(cipherText('world', 1, true), 'vnqkc');
+	assert.equal(cipherText('world', 1, true), 'vnqkc');
 })
 
 it('should return a value', () => {
-	assert.equeal(cipherText('hello', 0, true), 'hello');
+	assert.equal(cipherText('hello', 0, true), 'hello');
 })
 
 it('should return a value', () => {
-	assert.equeal(cipherText('hello', 0, false), 'hello');
+	assert.equal(cipherText('hello', 0, false), 'hello');
 })
 
 it('should return a value', () => {
-	assert.equeal(cipherText('hello', 2, true), 'fcjjm');
+	assert.equal(cipherText('hello', 2, true), 'fcjjm');
 })
 
 it('should return a value', () => {
-	assert.equeal(cipherText('hello', 2, false), 'jgnnq');
+	assert.equal(cipherText('hello', 2, false), 'jgnnq');
 })
