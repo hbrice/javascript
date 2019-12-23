e4cdfd970e4295e1aaae0791902c462ba22e01b6
hbrice <hn.brice@gmail.com>
2019-12-23 11:06:23 -0800

    updating unit tests for outliers and added chai ;

--- test.js
+++ test.js
@@ -1,41 +1,67 @@
 //unit tests for caesar cipher
-const ciphers = require('./caesar.js')
+const ciphers = require('./ciphers.js')
 
 const assert = require('assert')
+var chai = require('chai');
+var expect = chai.expect;
 
 
-it('should return true', () => {
-  assert.equal(true,true)
-})
+describe('Mocha Framework Tests ', function(){
 
-it('should return a value', () => {
-	assert.equal(ciphers.cipherText('lightsaber', 5, true), 'gdbconuvym');
-})
+	it('should return true', () => {
+	  assert.equal(true,true)
+	});
+});
 
-it('should return a value', () => {
-	assert.equal(ciphers.cipherText('lightsaber', 5, false), 'qnlmyxfgjw');
-})
+describe('Caesar Cipher Tests', function(){
 
-it('should return a value', () => {
-	assert.equal(ciphers.cipherText('world', 1, false), 'xpsme');
-})
+	it('should return a value for caesarCipher', () => {
+		assert.equal(ciphers.caesarCipher('lightsaber', 5, true), 'gdbconuvym');
+	})
 
-it('should return a value', () => {
-	assert.equal(ciphers.cipherText('world', 1, true), 'vnqkc');
-})
+	it('should return a value for caesarCipher', () => {
+		assert.equal(ciphers.caesarCipher('lightsaber', 5, false), 'qnlmyxfgjw');
+	})
 
-it('should return a value', () => {
-	assert.equal(ciphers.cipherText('hello', 0, true), 'hello');
-})
+	it('should return a value for caesarCipher', () => {
+		assert.equal(ciphers.caesarCipher('world', 1, false), 'xpsme');
+	})
 
-it('should return a value', () => {
-	assert.equal(ciphers.cipherText('hello', 0, false), 'hello');
-})
+	it('should return a value for caesarCipher', () => {
+		assert.equal(ciphers.caesarCipher('world', 1, true), 'vnqkc');
+	})
 
-it('should return a value', () => {
-	assert.equal(ciphers.cipherText('hello', 2, true), 'fcjjm');
-})
+	it('should return a value for caesarCipher', () => {
+		assert.equal(ciphers.caesarCipher('hello', 0, true), 'hello');
+	})
 
-it('should return a value', () => {
-	assert.equal(ciphers.cipherText('hello', 2, false), 'jgnnq');
-})
+	it('should return a value for caesarCipher', () => {
+		assert.equal(ciphers.caesarCipher('hello', 0, false), 'hello');
+	})
+
+	it('should return a value for caesarCipher', () => {
+		assert.equal(ciphers.caesarCipher('hello', 2, true), 'fcjjm');
+	})
+
+	it('should return a value for caesarCipher', () => {
+		assert.equal(ciphers.caesarCipher('hello', 2, false), 'jgnnq');
+	})
+
+	it('should return a value for caesarCipher', () => {
+		assert.equal(ciphers.caesarCipher('hello', 2, false), 'jgnnq');
+	})
+
+	it('should return a value for caesarCipher - large shiftCount', () => {
+		assert.equal(ciphers.caesarCipher('hello', 85, false), 'rovvy');
+	})
+
+
+	it('should throw an error for caesarCipher - no parameters', () => {
+		expect(function(){	ciphers.caesarCipher();	}).to.throw();
+	})
+
+	it('should throw an error for caesarCipher - no word', () => {
+		expect(function(){	ciphers.caesarCipher(1,true);	}).to.throw();
+	})
+
+});
