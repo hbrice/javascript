f2046c9790c5d59e35f985f3b6713ca19e74fe92
hbrice <hn.brice@gmail.com>
2019-12-23 10:18:34 -0800

    updating tests to call method cipherText

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 //unit tests for caesar cipher
-const caesar = require('./caesar.js')
+const ciphers = require('./caesar.js')
 
 const assert = require('assert')
 
@@ -9,33 +9,33 @@ it('should return true', () => {
 })
 
 it('should return a value', () => {
-	assert.equal(caesar.cipherText('lightsaber', 5, true), 'gdbconuvym');
+	assert.equal(ciphers.cipherText('lightsaber', 5, true), 'gdbconuvym');
 })
 
 it('should return a value', () => {
-	assert.equal(cipherText('lightsaber', 5, false), 'qnlmyxfgjw');
+	assert.equal(ciphers.cipherText('lightsaber', 5, false), 'qnlmyxfgjw');
 })
 
 it('should return a value', () => {
-	assert.equal(cipherText('world', 1, false), 'xpsme');
+	assert.equal(ciphers.cipherText('world', 1, false), 'xpsme');
 })
 
 it('should return a value', () => {
-	assert.equal(cipherText('world', 1, true), 'vnqkc');
+	assert.equal(ciphers.cipherText('world', 1, true), 'vnqkc');
 })
 
 it('should return a value', () => {
-	assert.equal(cipherText('hello', 0, true), 'hello');
+	assert.equal(ciphers.cipherText('hello', 0, true), 'hello');
 })
 
 it('should return a value', () => {
-	assert.equal(cipherText('hello', 0, false), 'hello');
+	assert.equal(ciphers.cipherText('hello', 0, false), 'hello');
 })
 
 it('should return a value', () => {
-	assert.equal(cipherText('hello', 2, true), 'fcjjm');
+	assert.equal(ciphers.cipherText('hello', 2, true), 'fcjjm');
 })
 
 it('should return a value', () => {
-	assert.equal(cipherText('hello', 2, false), 'jgnnq');
+	assert.equal(ciphers.cipherText('hello', 2, false), 'jgnnq');
 })
