73382a0a5903bd7a3109b352f4af2aa8e63f1a5f
hbrice <hn.brice@gmail.com>
2019-12-23 08:35:37 -0800

    Update caesar.js

--- caesar.js
+++ caesar.js
@@ -1,7 +1,7 @@
 // create a caesar cipher
 
 function cipherText(word, shiftCount){
-		const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+	const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 	const maxAlphabet = 25;
 
 	let encodedWord = ""; 	// String
